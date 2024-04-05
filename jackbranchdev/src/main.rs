5f3b9c0ca1af6c6c38c5e98275df1c02e4eb5175
= <branchjohnchristopher@gmail.com>
2024-04-04 23:14:06 -0500

    added askama, tokio, axum, etc and created web server

--- jackbranchdev/src/main.rs
+++ jackbranchdev/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
+use askama::Template;
+//use axum::routing::post;
+use axum::{response::IntoResponse, routing::get, Router};
+//use tower_http::services::{ServeDir, ServeFile};
+use tracing::{debug, info};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+mod html_template;
+use crate::html_template::HtmlTemplate;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // set up tracing for logging with defaults
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "seek_ql=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
     println!("Hello, world!");
+    let app = Router::new().route("/", get(index));
+
+    let port = 5173_u16;
+    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], port));
+
+    info!("Router initialized, now listening on port {}", port);
+
+    let listener = tokio::net::TcpListener::bind(addr)
+        .await
+        .expect("Failed to listen");
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to serve router");
+
+    Ok(())
 }
+
+async fn index() -> impl IntoResponse {
+    debug!("hit index!");
+    HtmlTemplate(IndexTemplate {})
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {}
